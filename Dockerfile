474b6fbbca8142d99ecdaf40a4777a612f3b7eb5
Amit Assaraf <amit@landa.app>
2023-02-08 17:33:36 +0200

    chore: full psycopg

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,4 @@ ARG python_ver
 FROM python:$python_ver
 
 WORKDIR /opt/psycopg
-RUN apt-get update && apt-get install -y libpq5
-RUN pip install psycopg -t .
+RUN pip install psycopg[binary] -t .
--- create_version.sh
+++ create_version.sh
@@ -1,13 +1,8 @@
 docker build -f Dockerfile --platform linux/amd64 . -t amitassaraf/psycopg-lambda  --build-arg python_ver=$1
 docker create --platform linux/amd64 --name psycopglayer amitassaraf/psycopg-lambda
-rm -rf lib
-mkdir -p lib
-docker cp psycopglayer:/opt/psycopg/psycopg .
-# Copy all the needed library files
-
-docker cp -a psycopglayer:/usr/lib/x86_64-linux-gnu/. lib/
-
 PY_EXT=$(echo $1 | tr . _)
-mv psycopg psycopg_$PY_EXT
+rm -rf psycopg_$PY_EXT
+mkdir -p psycopg_$PY_EXT
+docker cp psycopglayer:/opt/psycopg/. psycopg_$PY_EXT/
 docker rm -f psycopglayer
 
