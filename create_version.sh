a9223d20c042d4f2853356285b3ba1e907ded270
Amit Assaraf <amit@landa.app>
2023-02-08 17:22:33 +0200

    chore: test all libs

--- create_version.sh
+++ create_version.sh
@@ -5,23 +5,7 @@ mkdir -p lib
 docker cp psycopglayer:/opt/psycopg/psycopg .
 # Copy all the needed library files
 
-docker cp psycopglayer:/usr/lib/x86_64-linux-gnu/libpq.a lib/libpq.a
-docker cp psycopglayer:/usr/lib/x86_64-linux-gnu/libpq.so.5 lib/libpq.so.5
-docker cp psycopglayer:/usr/lib/x86_64-linux-gnu/libpq.so lib/libpq.so
-docker cp psycopglayer:/usr/lib/x86_64-linux-gnu/libpq.so.5.13 lib/libpq.so.5.13
-docker cp psycopglayer:/usr/lib/x86_64-linux-gnu/libssl.a lib/libssl.a
-docker cp psycopglayer:/usr/lib/x86_64-linux-gnu/libssl.so lib/libssl.so
-docker cp psycopglayer:/usr/lib/x86_64-linux-gnu/libssl.so.1.1 lib/libssl.so.1.1
-docker cp psycopglayer:/usr/lib/x86_64-linux-gnu/libcrypt.a lib/libcrypt.a
-docker cp psycopglayer:/usr/lib/x86_64-linux-gnu/libcrypt.so lib/libcrypt.so
-docker cp psycopglayer:/usr/lib/x86_64-linux-gnu/libcrypto.a lib/libcrypto.a
-docker cp psycopglayer:/usr/lib/x86_64-linux-gnu/libcrypto.so lib/libcrypto.so
-docker cp psycopglayer:/usr/lib/x86_64-linux-gnu/libcrypto.so.1.1 lib/libcrypto.so.1.1
-docker cp psycopglayer:/usr/lib/x86_64-linux-gnu/libldap-2.4.so.2 lib/libldap-2.4.so.2
-docker cp psycopglayer:/usr/lib/x86_64-linux-gnu/libldap_r-2.4.so.2 lib/libldap_r-2.4.so.2
-docker cp psycopglayer:/usr/lib/x86_64-linux-gnu/libldap_r-2.4.so.2.11.5 lib/libldap_r-2.4.so.2.11.5
-docker cp psycopglayer:/usr/lib/x86_64-linux-gnu/liblber-2.4.so.2.11.5 lib/liblber-2.4.so.2.11.5
-docker cp psycopglayer:/usr/lib/x86_64-linux-gnu/liblber-2.4.so.2 lib/liblber-2.4.so.2
+docker cp -a psycopglayer:/usr/lib/x86_64-linux-gnu/. lib/
 
 PY_EXT=$(echo $1 | tr . _)
 mv psycopg psycopg_$PY_EXT
